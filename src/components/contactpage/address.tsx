831515d8dd3fade8336c8e41f51fe51377f5a26d
Jestin Issac James <jestin.i.james@gmail.com>
2024-05-06 19:19:57 +0530

    feat(maps): figured to add a iframe map location

--- src/components/contactpage/address.tsx
+++ src/components/contactpage/address.tsx
@@ -1,5 +1,4 @@
 import { ArrowUpRightSquare } from 'lucide-react';
-import Image from 'next/image';
 import Link from 'next/link';
 
 import { contactUsContent } from '@/constant/config';
@@ -44,7 +43,7 @@ export function Address() {
             </div>
             <div className='w-full px-4 md:w-7/12'>
               <div className='my-4'>
-                <Link
+                {/* <Link
                   href={contactUsContent.location}
                   rel='noopener noreferrer'
                   target='_blank'
@@ -62,7 +61,18 @@ export function Address() {
                     data-srcset=''
                     sizes='100vw'
                   />
-                </Link>
+                </Link> */}
+                <div className='google-map-area'>
+                  <div className='mapouter'>
+                    <div className='gmap_canvas'>
+                      <iframe
+                        className='w-full h-[50vh]'
+                        id='gmap_canvas'
+                        src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.7575691825377!2d77.6180864!3d12.987351799999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae168d9128e881%3A0x432e6df0f40fd9ca!2sWorld%20Cassette%20Outreach%20of%20India%20(WCOI)!5e0!3m2!1sen!2sin!4v1715003269293!5m2!1sen!2sin'
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
